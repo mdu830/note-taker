5c59834fecdcb79352caaa6adfba71301816f5c4
mdu830 <mdu830@yahoo.com>
2021-02-25 16:02:36 -0500

    fixed typo in delete

--- routes/APIroute.js
+++ routes/APIroute.js
@@ -34,16 +34,16 @@ dataGet = (app) => {
     });
 
     app.delete("/api/notes", (req, res) => {
-        
-        id = req.params.id
+
+        id = req.params.id.toString()
 
         for (i=0; i < noteData.length; i++){
            
             if (noteData[i].id == id){
 
-                res.send(notesData[i]);
+                res.send(noteData[i]);
 
-                // Removes the deleted note
+                
                 noteData.splice(i,1);
                 break;
             }
